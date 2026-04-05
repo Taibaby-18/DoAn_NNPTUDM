e93e1a4209828d2beabfdbffdce566f02c2621f4
Taibaby-18 <ngotantai18092004@gmail.com>
2026-04-05 18:33:40 +0700

    merge tai sang main

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { upload } = require('../middleware/upload');
+const { upload,uploadGameMedia } = require('../middleware/upload');
 const uploadController = require('../controllers/uploadController');
 const router = express.Router();
 
@@ -34,6 +34,44 @@ router.post('/media', upload.single('file'), async function (req, res, next) {
     res.json({ message: 'File uploaded successfully', mediaAsset });
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+router.post('/game/:gameId', uploadGameMedia, async function (req, res, next) {
+  try {
+    const gameId = req.params.gameId; 
+    const files = req.files; // Lưu ý: upload nhiều file thì dùng req.files (có chữ s)
+
+    if (!files || Object.keys(files).length === 0) {
+      return res.status(400).json({ success: false, message: 'Vui lòng upload file!' });
+    }
+
+    const uploadedAssets = [];
+
+    // Xử lý Thumbnail
+    if (files.thumbnail) {
+      const asset = await uploadController.UploadMedia(gameId, 'Game', 'Thumbnail', `/uploads/${files.thumbnail[0].filename}`);
+      uploadedAssets.push(asset);
+    }
+
+    // Xử lý Trailer
+    if (files.trailer) {
+      const asset = await uploadController.UploadMedia(gameId, 'Game', 'Trailer', `/uploads/${files.trailer[0].filename}`);
+      uploadedAssets.push(asset);
+    }
+
+    // Xử lý Gallery (nhiều ảnh)
+    if (files.gallery) {
+      for (const file of files.gallery) {
+        const asset = await uploadController.UploadMedia(gameId, 'Game', 'Gallery', `/uploads/${file.filename}`);
+        uploadedAssets.push(asset);
+      }
+    }
+
+    res.status(200).json({ success: true, message: 'Upload media cho Game thành công!', data: uploadedAssets });
+  } catch (error) {
+    console.error("LỖI:", error);
+    res.status(500).json({ success: false, message: 'Lỗi server' });
   }
 });
 
